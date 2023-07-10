2a099ff841121354ece3a1ec6b6f2d038d071390
Lucas Gusmao <89878660+lucasgusmao98@users.noreply.github.com>
2023-07-10 18:41:19 +0000

    completed

---
+++ src/js/component/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <h2>Developed by Lucas Gusmao 2023</h2>
+    </div>
+  )
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
+import Footer from "./footer";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -7,18 +10,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar/> 
+			<Jumbotron/>
+			<div className= "d-flex justify-content-center">
+				<Card/>
+				<Card title={"Cities"}/>
+				<Card img={"https://img.bleacherreport.net/img/images/photos/002/975/442/hi-res-6590d5d897933931840ca893be28ab4f_crop_north.jpg?1405281115&w=3072&h=2048"}/>
+				<Card/>
+			
+			</div>
+			<Footer/>
 		</div>
 	);
 };
